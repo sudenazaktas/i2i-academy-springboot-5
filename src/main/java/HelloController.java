ae5802c632b71e65b1584a97419e65e9495491a9
Sude Naz Aktas <snazaktas@gmail.com>
2026-07-01 19:37:10 +0300

    Add Spring Boot project with Hello World REST endpoint

---
+++ src/main/java/HelloController.java
@@ -0,0 +1,14 @@
+package com.i2iacademyspring.springboot_demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Welcome to i2i Academy!";
+    }
+
+}
